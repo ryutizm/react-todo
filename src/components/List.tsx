534a58ff9fedfacac270a43bbc1885da341c4c3b
ryutizm <ryutizm1103@gmail.com>
2023-03-07 14:22:15 +0900

    create Todo App

---
+++ src/components/List.tsx
@@ -0,0 +1,35 @@
+import { Item } from "./Item";
+import { Todo } from "./Todo";
+
+// 受け取るPropsの型を定義する
+type Listprops = {
+    todos: Todo[];
+    deleteTodo: Function;
+    updateTodo: Function;
+};
+
+const List: React.FC<Listprops> = ({ todos, deleteTodo, updateTodo }) => {
+    // 押したTODOのidを[deleteTodo]に渡す
+    const complete = (id: number) => {
+        deleteTodo(id);
+    };
+
+    return (
+        <div>
+            {/* TODOリストを取得して[map関数]で1つ1つ取り出す */}
+            {todos.map((todo) => {
+                return (
+                    // [Item]に必要なPropsを取り出す
+                    <Item
+                        key={todo.id}
+                        todo={todo}
+                        complete={complete}
+                        updateTodo={updateTodo}
+                    />
+                );
+            })}
+        </div>
+    );
+};
+
+export default List;
